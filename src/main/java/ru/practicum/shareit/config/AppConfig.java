079f56abdaa55726537621e1d806257c2fa2724b
Dmitriy Kandalov <i@dkandalov.ru>
2022-08-30 22:27:40 +0500

    feat: work

--- src/main/java/ru/practicum/shareit/config/AppConfig.java
+++ src/main/java/ru/practicum/shareit/config/AppConfig.java
@@ -18,47 +18,6 @@ import java.util.Properties;
 @Configuration
 @PropertySource("classpath:/application.properties")
 public class AppConfig {
-    private final Environment environment;
-
-    public AppConfig(Environment environment) {
-        this.environment = environment;
-    }
-
-    private Properties hibernateProperties() {
-        Properties properties = new Properties();
-        properties.put("hibernate.dialect", environment.getRequiredProperty("hibernate.dialect"));
-        properties.put("hibernate.show_sql", environment.getRequiredProperty("hibernate.show_sql"));
-        return properties;
-    }
-
-    @Bean
-    public DataSource dataSource() {
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName(environment.getRequiredProperty("jdbc.driverClassName"));
-        dataSource.setUrl(environment.getRequiredProperty("jdbc.url"));
-        dataSource.setUsername(environment.getRequiredProperty("jdbc.username"));
-        dataSource.setPassword(environment.getRequiredProperty("jdbc.password"));
-        return dataSource;
-    }
-
-    @Bean
-    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource) {
-        final LocalContainerEntityManagerFactoryBean emf = new LocalContainerEntityManagerFactoryBean();
-        emf.setDataSource(dataSource);
-        emf.setPackagesToScan("ru.practicum.shareit");
-        final HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
-        emf.setJpaVendorAdapter(vendorAdapter);
-        emf.setJpaProperties(hibernateProperties());
-
-        return emf;
-    }
-
-    @Bean
-    public JpaTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
-        JpaTransactionManager transactionManager = new JpaTransactionManager();
-        transactionManager.setEntityManagerFactory(entityManagerFactory);
-        return transactionManager;
-    }
 
 }
 
--- src/main/java/ru/practicum/shareit/item/itemRepository/ItemRepository.java
+++ src/main/java/ru/practicum/shareit/item/itemRepository/ItemRepository.java
@@ -1,7 +1,6 @@
 package ru.practicum.shareit.item.itemRepository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import ru.practicum.shareit.item.model.Item;
 
 public interface ItemRepository extends JpaRepository<Item, Long> {
