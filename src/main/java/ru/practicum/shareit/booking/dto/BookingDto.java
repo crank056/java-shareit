879af6b2fca7538a5956ec6738ed4f1baa4e7521
Dmitriy Kandalov <i@dkandalov.ru>
2022-10-06 20:38:52 +0500

    feat: checkstyle 12

--- src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
+++ src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
@@ -2,7 +2,6 @@ package ru.practicum.shareit.booking.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 import ru.practicum.shareit.item.dto.ItemDto;
 import ru.practicum.shareit.user.dto.UserDto;
 import ru.practicum.shareit.util.Status;
